8ead8b8fbaf56cdf979ce88834b8e0636f2cdb43
lmx <limx@deepcool.com>
2023-05-15 11:25:44 +0800

    feat: 将硬件信息转换成json字符串

--- LibreHardwareMonitor/LibreHardwareMonitorLib/Entrypoint/Entrypoint.cs
+++ LibreHardwareMonitor/LibreHardwareMonitorLib/Entrypoint/Entrypoint.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections;
 using LibreHardwareMonitor.Hardware;
+using Newtonsoft.Json;
 
 namespace LibreHardwareMonitor.Entrypoint
 {
@@ -17,31 +20,70 @@ namespace LibreHardwareMonitor.Entrypoint
                 IsNetworkEnabled = true,
                 IsStorageEnabled = true
             };
-        }
-
-        public void GetComputerMessage() {
             _computer.Open();
             _computer.Accept(new UpdateVisitor());
+        }
 
+        private String GetSensorType(SensorType sensorType) {
+            switch (sensorType)
+            {
+                case SensorType.Clock: return "CLOCK";
+                case SensorType.Control: return "CONTROL";
+                case SensorType.Current: return "CURRENT";
+                case SensorType.Data: return "DATA";
+                case SensorType.Energy: return "ENERGY";
+                case SensorType.Factor: return "FACTOR";
+                case SensorType.Fan: return "FAN";
+                case SensorType.Flow: return "FLOW";
+                case SensorType.Frequency: return "FREQUENCY";
+                case SensorType.Level: return "LEVEL";
+                case SensorType.Load: return "LOAD";
+                case SensorType.Noise: return "NOISE";
+                case SensorType.Power: return "POWER";
+                case SensorType.SmallData: return "SMALLDATA";
+                case SensorType.Temperature: return "TEMPERATURE";
+                case SensorType.Throughput: return "THROUGHPUT";
+                case SensorType.TimeSpan: return "TIMESPAN";
+                default: return "VOLTAGE";
+            }
+        }
+
+        public String GetComputerMessage() {
+            Dictionary<String, ArrayList> HardwardMap = new Dictionary<string, ArrayList>();
             foreach (IHardware hardware in _computer.Hardware)
             {
-                Console.WriteLine("Hardware: {0}", hardware.Name);
-
+                ArrayList HardwareList = new ArrayList();
+                HardwardMap.Add(hardware.Name, HardwareList); //最外层
                 foreach (IHardware subhardware in hardware.SubHardware)
                 {
-                    Console.WriteLine("\tSubhardware: {0}", subhardware.Name);
-
+                    ArrayList SubHardwareList = new ArrayList();
+                    Dictionary<String, ArrayList> SubHardwareMap = new Dictionary<String, ArrayList>();
+                    SubHardwareMap.Add(subhardware.Name, SubHardwareList);
                     foreach (ISensor sensor in subhardware.Sensors)
                     {
-                        Console.WriteLine("\t\tSensor: {0}, value: {1}", sensor.Name, sensor.Value);
+                        Dictionary<String, Object> SensorMap = new Dictionary<String, Object>();
+                        SensorMap.Add("Name", sensor.Name);
+                        SensorMap.Add("Type", GetSensorType(sensor.SensorType));
+                        SensorMap.Add("Value", sensor.Value);
+                        SensorMap.Add("Min", sensor.Min);
+                        SensorMap.Add("Max", sensor.Max);
+                        SubHardwareList.Add(SensorMap);
                     }
                 }
 
                 foreach (ISensor sensor in hardware.Sensors)
                 {
-                    Console.WriteLine("\tSensor: {0}, value: {1}", sensor.Name, sensor.Value);
+                    Dictionary<String, Object> SensorMap = new Dictionary<String, Object>();
+                    SensorMap.Add("Name", sensor.Name);
+                    SensorMap.Add("Type", GetSensorType(sensor.SensorType));
+                    SensorMap.Add("Value", sensor.Value);
+                    SensorMap.Add("Min", sensor.Min);
+                    SensorMap.Add("Max", sensor.Max);
+                    HardwareList.Add(SensorMap);
                 }
             }
+
+            return JsonConvert.SerializeObject(HardwardMap);
         }
     }
 }
---
+++ LibreHardwareMonitor/LibreHardwareMonitorTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using LibreHardwareMonitor.Entrypoint;
+
+
+namespace LibreHardwareMonitorTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NodeLibreHardwareMonitorLib nodeLibreHardwareMonitorLib = new NodeLibreHardwareMonitorLib();
+            string result = nodeLibreHardwareMonitorLib.GetComputerMessage();
+            Console.WriteLine(result);
+        }
+    }
+}
